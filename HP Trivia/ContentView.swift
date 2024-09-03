48dd1cb902ff6acbca5db3a789fd01b4f945f084
Kyaw-Zin-George <ktz1695@gmail.com>
2024-09-03 14:21:32 +0100

    Initial Commit

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -14,6 +14,76 @@ struct ContentView: View {
                 Image("hogwarts")
                     .resizable()
                     .frame(width: geo.size.width * 3, height: geo.size.height)
+                
+                VStack{
+                    VStack{
+                        Image(systemName: "bolt.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                        
+                        Text("HP")
+                            .font(.custom(Constant.hpFont,size: 70))
+                        //to get closer to hp and trivia
+                            .padding(.bottom, -50)
+                        Text("Trivia")
+                            .font(.custom(Constant.hpFont, size: 60))
+                    }.padding(.top, 70)
+                    
+                    Spacer()
+                    
+                    VStack{
+                        Text("Recent Scores:")
+                            .font(.title2)
+                        Text("33")
+                        Text("53")
+                        Text("20")
+                    }
+                    .font(.title3)
+                    .padding(.horizontal)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.7))
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    Spacer()
+                    
+                    HStack{
+                        Spacer()
+                        Button{
+                            //
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                        
+                        Button{
+                            
+                        }label: {
+                            Text("Play")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 50)
+                                .background(.brown)
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                        }
+                        
+                        Spacer()
+                        
+                        Button{
+                            
+                        }label: {
+                            Image(systemName: "gearshape.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        Spacer()
+                    }.frame(width: geo.size.width)
+                    Spacer()
+                }
             }
             .frame(width: geo.size.width, height: geo.size.height)
             
