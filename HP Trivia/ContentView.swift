f27415437422c617acb639c56857c54f2c3eb53e
Kyaw-Zin-George <ktz1695@gmail.com>
2024-09-03 14:21:32 +0100

    Initial Commit

--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -14,6 +14,21 @@ struct ContentView: View {
                 Image("hogwarts")
                     .resizable()
                     .frame(width: geo.size.width * 3, height: geo.size.height)
+                
+                VStack{
+                    VStack{
+                        Image(systemName: "bolt.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                        
+                        Text("HP")
+                            .font(.custom(Constant.hpFont,size: 70))
+                        //to get closer to hp and trivia
+                            .padding(.bottom, -50)
+                        Text("Trivia")
+                            .font(.custom(Constant.hpFont, size: 60))
+                    }
+                }
             }
             .frame(width: geo.size.width, height: geo.size.height)
             
