70f5a045533d50fcdffc873d5ef390b80dd866bd
Kyaw-Zin-George <ktz1695@gmail.com>
2024-09-04 12:17:56 +0100

    Create Instructions Screen along with creating some constants and extensions for avoiding repition of codes

---
+++ HP Trivia/Views/Instructions.swift
@@ -0,0 +1,18 @@
+//
+//  Instructions.swift
+//  HP Trivia
+//
+//  Created by Kyaw Thant Zin(George) on 9/4/24.
+//
+
+import SwiftUI
+
+struct Instructions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Instructions()
+}
